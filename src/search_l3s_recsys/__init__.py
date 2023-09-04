eb95d8db1fa784e103ed5a0e48b6d53159c86f85
Peng-LUH <ethaniel.unihannover@gmail.com>
2023-09-04 15:58:45 +0200

    update

--- src/search_l3s_recsys/__init__.py
+++ src/search_l3s_recsys/__init__.py
@@ -1,10 +1,11 @@
 """Flask app initialization via factory pattern."""
 
-from flask import Flask
+from flask import Flask, redirect
 from flask_bcrypt import Bcrypt
 from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 from search_l3s_recsys.config import get_config
 
@@ -18,6 +19,9 @@ def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(get_config(config_name))
 
+
+    
+    
     # to avoid a circular import
     from search_l3s_recsys.api import api_bp
     
@@ -26,4 +30,8 @@ def create_app(config_name):
     db.init_app(app)
     migrate.init_app(app, db)
     bcrypt.init_app(app)
+    
+    @app.route('/')
+    def index():
+        return redirect(f"http://{os.getenv('HOST_IP')}:{os.getenv('L3S_SEARCH_PORT')}/l3s-search/", code=200)
     return app
